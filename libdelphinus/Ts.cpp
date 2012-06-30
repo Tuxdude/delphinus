3b806cad1d3a312c8bb892821c1a78f626ba59f4
Ash <tuxdude@WingSaber>
2012-06-29 18:57:21 -0700

    Moved from GPL v2 -> LGPL v3 licensing.
    Moved the whole set of files in the delphinus project to the more
    relaxed LGPL v3 licensing.

--- libdelphinus/Ts.cpp
+++ libdelphinus/Ts.cpp
@@ -2,24 +2,23 @@
  *  Ts.cpp - declaration of types related to transport stream header
  *  and the fields within as specified in the ISO 13818-1 document
  *
- *  This file is part of delphinus - a stream analyzer for various multimedia
- *  stream formats and containers.
+ *  This file is part of delphinus.
  *
- *  Copyright (C) 2012 Ash (Tuxdude)
- *  tuxdude.github@gmail.com
+ *  Copyright (C) 2012 Ash (Tuxdude) <tuxdude.github@gmail.com>
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation; either version 3 of the
+ *  License, or (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program.  If not, see
+ *  <http://www.gnu.org/licenses/>.
  */
 
 #include "Ts.h"
